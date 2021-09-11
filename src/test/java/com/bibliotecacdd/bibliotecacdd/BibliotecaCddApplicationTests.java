19ad0ded1c4256a3490d2a55e9b0a08b17ac3f7a
ppfurtado <furtado.pedropaulo@gmail.com>
2021-09-10 23:31:52 -0300

    Create Project Base

---
+++ src/test/java/com/bibliotecacdd/bibliotecacdd/BibliotecaCddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibliotecacdd.bibliotecacdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaCddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
